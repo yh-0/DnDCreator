c215230e6cd9aeba96e2df222f72cb37fa54712a
Gracjan Sadowicz <gracjan@ravendb.net>
2023-06-02 21:32:29 +0200

    Use the cls in classmethods

--- dice.py
+++ dice.py
@@ -3,20 +3,20 @@ import random
 
 class Dice:
     @classmethod
-    def roll(self, size, count=1):
+    def roll(cls, size, count=1):
         rolls = [random.randint(1, size) for _ in range(count)]
         return sum(rolls), rolls
 
     @classmethod
-    def show_single(self, roll):
+    def show_single(cls, roll):
         return f"{roll[0]} {roll[1]}"
 
     @classmethod
-    def roll_stats(self):
-        rolls = [self.roll(6, 4) for _ in range(6)]
+    def roll_stats(cls):
+        rolls = [cls.roll(6, 4) for _ in range(6)]
         lowest_removed = [sorted(roll[1])[1:] for roll in rolls]
         return [(sum(roll), roll) for roll in lowest_removed]
 
     @classmethod
-    def show_rolls(self, rolls):
+    def show_rolls(cls, rolls):
         return "\n".join([f"{roll[0]:<{7 - len(str(roll[0]))}} {roll[1]}" for roll in rolls])
